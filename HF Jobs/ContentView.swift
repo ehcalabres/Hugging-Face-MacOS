0881c69e04c4932e9519a9f976e41456a25f8726
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2026-08-05 18:52:32 +0200

    Initial Commit

---
+++ HF Jobs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HF Jobs
+//
+//  Created by Enrique Hernández Calabrés on 05/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HF Jobs/HF_JobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HF_JobsApp.swift
+//  HF Jobs
+//
+//  Created by Enrique Hernández Calabrés on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct HF_JobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
